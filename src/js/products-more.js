e2dc74ab73bd3987bba7ad4f5104128bb97388be
Dima1409 <Dmytrolevchenko22@gmail.com>
2022-06-28 21:59:23 +0300

    js

--- src/js/products-more.js
+++ src/js/products-more.js
@@ -17,15 +17,6 @@
 
   openMenuBtn.addEventListener('click', toggleMenu);
   closeMenuBtn.addEventListener('click', toggleMenu);
-
-  // Закрываем мобильное меню на более широких экранах
-  // в случае изменения ориентации устройства.
-  //   window.matchMedia('(min-width: 768px)').addEventListener('change', e => {
-  //     if (!e.matches) return;
-  //     mobileMenu.classList.remove('is-open');
-  //     openMenuBtn.setAttribute('aria-expanded', false);
-  //     bodyScrollLock.enableBodyScroll(document.body);
-  //   });
 })();
 
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -49,15 +40,6 @@
 
   openMenuBtn.addEventListener('click', toggleMenu);
   closeMenuBtn.addEventListener('click', toggleMenu);
-
-  // Закрываем мобильное меню на более широких экранах
-  // в случае изменения ориентации устройства.
-  //   window.matchMedia('(min-width: 768px)').addEventListener('change', e => {
-  //     if (!e.matches) return;
-  //     mobileMenu.classList.remove('is-open');
-  //     openMenuBtn.setAttribute('aria-expanded', false);
-  //     bodyScrollLock.enableBodyScroll(document.body);
-  //   });
 })();
 /////////////////////////////////////////////////////////////////////////////////////////////////////////
 
@@ -80,13 +62,4 @@
 
   openMenuBtn.addEventListener('click', toggleMenu);
   closeMenuBtn.addEventListener('click', toggleMenu);
-
-  // Закрываем мобильное меню на более широких экранах
-  // в случае изменения ориентации устройства.
-  //   window.matchMedia('(min-width: 768px)').addEventListener('change', e => {
-  //     if (!e.matches) return;
-  //     mobileMenu.classList.remove('is-open');
-  //     openMenuBtn.setAttribute('aria-expanded', false);
-  //     bodyScrollLock.enableBodyScroll(document.body);
-  //   });
 })();
